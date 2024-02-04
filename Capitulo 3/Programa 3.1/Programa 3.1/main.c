3d9b410c683ac5618d80b4688655673c826904f0
sandydeoleo <sandydeoleo378@gmail.com>
2024-02-04 18:39:44 -0400

    guardado 4

---
+++ Capitulo 3/Programa 3.1/Programa 3.1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*Nomina.
+El programa, al recibir los salarios de 5 profesore, obtiene el total de la nomina de la universidad.
+
+I: variable de tipo entero.
+SAL y NOM: variable de tipo real. */
+
+void main(void)
+{
+    int I;
+    float SAL, NOM;
+    NOM = 0;
+    for (I=1 ; I<=5; I++)
+    {
+        printf("Ingrese el salario del profesor%d:\t", I);
+        scanf("%f", &SAL);
+        NOM = NOM + SAL;
+    }
+    printf("\nEl total de la nomina es: %.2f", NOM);
+}
